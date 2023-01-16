8d883d27351557e77103f69e8f4f73b0c88a5052
batman <2advancesolutions@gmail.com>
2023-01-16 16:14:50 -0600

    add lambda stack training

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myLambda = new Function(this, 'MyLambda', {
+      runtime: Runtime.NODEJS_16_X,
+      memorySize: 512,
+      code: Code.fromAsset(join(__dirname, '../lambdas')),
+      handler: 'app.handler'
+    });
   }
 }
